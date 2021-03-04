ae0d0eee5ec82149a94e6c82627a600ca49b4942
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-03-04 11:14:20 -0500

    Init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import { spawn } from 'child_process'
+import * as fs from "fs";
+import { v1 } from 'uuid'
+const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.post('/', (req, res) => {
+    const uuid = v1()
+    const genObj = spawn('java', [
+        `-jar`,
+        `./jmc2obj.jar`,
+        `./users/rokiseth`,
+        `--area=${req.query.minx},${req.query.minz},${req.query.maxx},${req.query.maxz}`,
+        `--objfile=../htdocs/navis/${uuid}.obj`,
+        `--mtlfile=../htdocs/navis/${uuid}.mtl`,
+        `--optimize-geometry`,
+        `--remove-dup`,
+    ]).on('exit', () => {
+        res.json({
+            id: uuid
+        })
+    })
+    genObj.stdout.on('data', c => console.log(c.toString()))
+})
+
+app.listen(4000, () => {
+    console.log("Start!")
+})
